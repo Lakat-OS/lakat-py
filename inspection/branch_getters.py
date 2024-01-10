c06c25c8feee6039b16b17d3ab7bd9630c86e99e
Leonhard Horstmeyer <leo.h@hotmail.de>
2024-01-10 21:52:08 +0200

    wip on branch_getters

---
+++ inspection/branch_getters.py
@@ -0,0 +1,9 @@
+import lakat.storage as lakat_storage
+
+def get_article_id_from_article_name(branch_id: bytes, name: str):
+    ## get the branch head
+
+
+def get_branch_head_id(branch_id: bytes):
+    ## get the branch head
+    db_get
