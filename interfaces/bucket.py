41ae3414fb9e99fce87efa915e5bf704fd6f585e
Leonhard Horstmeyer <leo.h@hotmail.de>
2024-01-12 01:29:31 +0200

    now each bucket and each article has a root

--- interfaces/bucket.py
+++ interfaces/bucket.py
@@ -1,11 +1,12 @@
 from typing import List
 
 class BUCKET:
-    def __init__(self, schema_id: int, signature: bytes, public_key: bytes, parent_bucket: bytes, data: bytes, refs: bytes, timestamp: int):
+    def __init__(self, schema_id: int, signature: bytes, public_key: bytes, parent_bucket: bytes, root_bucket: bytes, data: bytes, refs: bytes, timestamp: int):
         self.schema_id = schema_id
         self.signature = signature
         self.public_key = public_key
         self.parent_bucket = parent_bucket
+        self.root_bucket = root_bucket
         self.data = data
         self.refs = refs
         self.timestamp = timestamp
